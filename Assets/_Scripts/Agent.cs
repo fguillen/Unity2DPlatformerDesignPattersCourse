1285307e5b23d0f13c057dd2862261ba97f1fcdd
Fernando Guillen <fguillen.mail@gmail.com>
2022-09-21 10:45:44 +0200

    Player idle and run animations

--- Assets/_Scripts/Agent.cs
+++ Assets/_Scripts/Agent.cs
@@ -6,11 +6,13 @@ public class Agent : MonoBehaviour
 {
     Rigidbody2D rb2d;
     PlayerInput playerInput;
+    AgentAnimation agentAnimation;
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         playerInput = GetComponentInParent<PlayerInput>();
+        agentAnimation = GetComponentInChildren<AgentAnimation>();
     }
 
     // Start is called before the first frame update
@@ -27,6 +29,12 @@ public class Agent : MonoBehaviour
 
     void HandleMovement(Vector2 movement)
     {
+        if(Mathf.Abs(movement.x) > 0.01f)
+            agentAnimation.PlayAnimation(AnimationType.run);
+        else
+            agentAnimation.PlayAnimation(AnimationType.idle);
+
+
         rb2d.velocity = new Vector2(movement.x * 5f, rb2d.velocity.y);
     }
 }
---
+++ Assets/_Scripts/AgentAnimation.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAnimation : MonoBehaviour
+{
+    Animator animator;
+    AnimationType animationActive;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation(AnimationType animationType)
+    {
+        if(animationActive == animationType) return;
+
+        switch (animationType)
+        {
+            case AnimationType.idle:
+                Play("Idle");
+                break;
+            case AnimationType.run:
+                Play("Run");
+                break;
+
+            default:
+                break;
+        }
+
+        animationActive = animationType;
+    }
+
+    void Play(string name)
+    {
+        animator.Play(name, -1, 0f);
+    }
+}
+
+public enum AnimationType
+{
+    die,
+    hit,
+    idle,
+    attack,
+    run,
+    jump,
+    fall,
+    climb,
+    land
+}
