0a0170a8606c394d1f9b6c05007eb66f0714e8a5
Fernando Guillen <fguillen.mail@gmail.com>
2022-10-13 18:53:37 +0200

    MainMenu: Clouds

---
+++ Assets/_Scripts/Common/RandomUtils.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class RandomUtils
+{
+    public static Vector2 RandomPointInBox(BoxCollider2D collider)
+    {
+        // var center = (Vector2)collider.transform.localPosition + collider.offset;
+        var center = collider.bounds.center;
+        var size = collider.bounds.size;
+
+        float x = UnityEngine.Random.Range(center.x - size.x, center.x + size.x);
+        float y = UnityEngine.Random.Range(center.y - size.y, center.y + size.y);
+
+        Vector2 result = new Vector2(x, y);
+
+        Debug.Log($"RandomUtils: {center}, {size}, {result}");
+
+        return result;
+    }
+}
---
+++ Assets/_Scripts/MainMenu/Cloud.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using DG.Tweening;
+
+public class Cloud : MonoBehaviour
+{
+    [SerializeField] List<Sprite> sprites;
+    [SerializeField] float speedMin = 10f;
+    [SerializeField] float speedMax = 20f;
+    [SerializeField] float scaleMin = 0.1f;
+    [SerializeField] float scaleMax = 3f;
+
+    [SerializeField] BoxCollider2D respawnCollider;
+
+    Image image;
+    RectTransform imageRectTransform;
+    float speed;
+    float scale;
+
+    void Awake()
+    {
+        image = GetComponentInChildren<Image>();
+        imageRectTransform = image.GetComponent<RectTransform>();
+        respawnCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        image.sprite = sprites[Random.Range(0, sprites.Count)];
+        speed = Random.Range(speedMin, speedMax);
+        scale = Random.Range(scaleMin, scaleMax);
+
+        imageRectTransform.localScale = Vector3.one * scale;
+        imageRectTransform
+            .DOMoveX(Screen.width + image.sprite.bounds.size.x, Screen.width / speed)
+            .SetEase(Ease.Linear)
+            .OnComplete(ReSpawn);
+    }
+
+    void ReSpawn()
+    {
+        DOTween.Kill(imageRectTransform);
+        Vector2 position = RandomUtils.RandomPointInBox(respawnCollider);
+
+        imageRectTransform.position = position;
+
+        Debug.Log($"position: {position}, {imageRectTransform.position}");
+
+        Initialize();
+    }
+}
